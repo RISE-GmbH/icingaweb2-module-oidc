0ab4060cf334914c9e606b84779a59be5ebfdb9b
moreamazingnick <github@nicolas-schneider.at>
2025-07-23 15:38:48 +0200

    Update 0.5.6.sql

--- schema/mysql-upgrades/0.5.6.sql
+++ schema/mysql-upgrades/0.5.6.sql
@@ -1,14 +1,3 @@
-CREATE TABLE tbl_schema (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    version varchar(64) NOT NULL,
-    timestamp bigint unsigned NOT NULL,
-    success enum ('n', 'y') DEFAULT NULL,
-    reason text DEFAULT NULL,
-
-    PRIMARY KEY (id),
-    CONSTRAINT idx_tbl_schema_version UNIQUE (version)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
-
 ALTER TABLE tbl_provider ADD COLUMN syncgroups LONGTEXT DEFAULT NULL;
 ALTER TABLE tbl_provider ADD COLUMN defaultgroup TEXT DEFAULT NULL;
 ALTER TABLE tbl_provider ADD COLUMN usernameblacklist TEXT DEFAULT NULL;
